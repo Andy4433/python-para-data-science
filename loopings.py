79537b58a3605a6a093976dfeb93d132f64ca382
Anderson de Sousa Brito <135986605+Andy4433@users.noreply.github.com>
2023-07-12 18:48:37 -0300

    Create loopings.py

---
+++ loopings.py
@@ -0,0 +1,8 @@
+# Use the range
+range(3)  # Cria um objeto de intervalo que gera uma sequência de números de 0 a 3
+
+# Explicação do uso do range:
+# A função range cria um objeto de intervalo que representa uma sequência de números.
+# Neste caso, range(3) gera uma sequência de números de 0 a 3 (excluindo o número 3).
+# Podemos usar esse objeto de intervalo em iterações, como loops for, para percorrer os números da sequência.
+# Por exemplo, 'for i in range(3):' irá iterar sobre os números 0,3.
