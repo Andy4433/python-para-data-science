815593b7ebb8343460880421bf3a6c884dc2c3c1
Anderson de Sousa Brito <135986605+Andy4433@users.noreply.github.com>
2023-07-13 09:34:18 -0300

    Create Funçoes.py

---
+++ Funçoes.py
@@ -0,0 +1,9 @@
+
+def add(a):
+  # Adiciona 1 a 'a'
+  # Armazena a soma de 'a' + 1 em 'b'
+  b = a + 1
+  # Imprime o valor de 'a' e o valor de 'b' após a adição
+  print(a, "se você adicionar um", b)
+  # Retorna o valor de 'b'
+  return b
